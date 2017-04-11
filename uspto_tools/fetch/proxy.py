472093f3c88e2361c7baf36a178b9873cb70a722
RickardSjogren <r.sjogren89@gmail.com>
2017-04-11 14:19:35 +0200

    Removed pandas dependency

--- uspto_tools/fetch/proxy.py
+++ uspto_tools/fetch/proxy.py
@@ -3,9 +3,9 @@ fetches proxies.
 """
 import logging
 import requests
-from requests.exceptions import ProxyError
+from bs4 import BeautifulSoup
 
-import pandas as pd
+from requests.exceptions import ProxyError
 
 
 class ProxySession(requests.Session):
@@ -39,14 +39,20 @@ class USProxySession(ProxySession):
 
     def fetch_proxies(self):
         r = requests.get('https://www.us-proxy.org/')
-        tables = pd.read_html(r.text)
-        ip_table = next(table for table in tables
-                        if 'IP Address' in table.columns)
-        correct_level = ip_table[ip_table['Anonymity'] == self.level]
-        with_https = correct_level[correct_level['Https'] == 'yes']
-        most_fresh = with_https.iloc[0]
-        ip = most_fresh['IP Address']
-        port = int(most_fresh['Port'])
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, 'html.parser')
+        table = next(t for t in soup.find_all('table')
+                     if t.has_attr('id') and t['id'] == 'proxylisttable')
+        header = table.find('thead').text.strip().split('\n')
+        header_map = {h: i for i, h in enumerate(header)}
+
+        rows = table.find('tbody').find_all('tr')
+        rows_at_level = (row for row in rows if
+                         list(row)[header_map['Anonymity']].text == self.level)
+        first_at_level = next(rows_at_level)
+        ip = list(first_at_level)[header_map['IP Address']].text
+        port = list(first_at_level)[header_map['Port']].text
+
         proxies = {
             'http': 'http://{}:{}'.format(ip, port),
             'https': 'https://{}:{}'.format(ip, port)
